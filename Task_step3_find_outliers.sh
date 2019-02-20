f7cf4c3f03f9820f0cc823acf6e35b795b535bf2
nmuncy <pingamormn@gmail.com>
2019-02-20 10:45:45 -0700

    reml test update

--- Task_step3_find_outliers.sh
+++ Task_step3_find_outliers.sh
@@ -23,7 +23,7 @@ module load r/3/5
 
 
 ###??? update these
-workDir=~/compute/STT_bids/derivatives
+workDir=~/compute/STT_reml/derivatives
 grpDir=${workDir}/Analyses/grpAnalysis
 refDir=${workDir}/sub-1295
 maxP=0.1								# max percentage of censored TRs allowed (0.1 = 10%)
